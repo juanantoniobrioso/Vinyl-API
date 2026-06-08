eeda746ff6c3f8de82e47f5e5c208be3a62ca4b0
Juan Antonio <juanantoniobrioso.24@campuscamara.es>
2026-06-08 11:48:50 +0200

    first commit

---
+++ backend/vinilos.js
@@ -0,0 +1,24 @@
+const vinilos = [
+  { id: 1, titulo: "Abbey Road", artista: "The Beatles", genero: "Rock", anio: 1969, portada_url: "https://images.unsplash.com/photo-1614613535308-eb5fbd3d2c17?w=300" },
+  { id: 2, titulo: "What's Going On", artista: "Marvin Gaye", genero: "Soul", anio: 1971, portada_url: "https://images.unsplash.com/photo-1539628399213-d6aa89c93074?w=300" },
+  { id: 3, titulo: "Kind of Blue", artista: "Miles Davis", genero: "Jazz", anio: 1959, portada_url: "https://images.unsplash.com/photo-1511192336575-5a79af67a629?w=300" },
+  { id: 4, titulo: "Lady Soul", artista: "Aretha Franklin", genero: "Soul", anio: 1968, portada_url: "https://images.unsplash.com/photo-1514525253161-7a46d19cd819?w=300" },
+  { id: 5, titulo: "Texas Flood", artista: "Stevie Ray Vaughan", genero: "Blues", anio: 1983, portada_url: "https://images.unsplash.com/photo-1564186763535-ebb21ef5277f?w=300" },
+  { id: 6, titulo: "The Dark Side of the Moon", artista: "Pink Floyd", genero: "Rock", anio: 1973, portada_url: "https://images.unsplash.com/photo-1603048588665-791ca8aea617?w=300" },
+  { id: 7, titulo: "Songs in the Key of Life", artista: "Stevie Wonder", genero: "Soul", anio: 1976, portada_url: "https://images.unsplash.com/photo-1487180142328-0c4e37023af5?w=300" },
+  { id: 8, titulo: "Thriller", artista: "Michael Jackson", genero: "R&B", anio: 1982, portada_url: "https://images.unsplash.com/photo-1508700115892-45ecd05ae2ad?w=300" },
+  { id: 9, titulo: "Back to Black", artista: "Amy Winehouse", genero: "R&B", anio: 2006, portada_url: "https://images.unsplash.com/photo-1498038432885-c6f3f1b912ee?w=300" },
+  { id: 10, titulo: "Born to Run", artista: "Bruce Springsteen", genero: "Rock", anio: 1975, portada_url: "https://images.unsplash.com/photo-1459749411175-04bf5292ceea?w=300" },
+  { id: 11, titulo: "At Folsom Prison", artista: "Johnny Cash", genero: "Country", anio: 1968, portada_url: "https://images.unsplash.com/photo-1470225620780-dba8ba36b745?w=300" },
+  { id: 12, titulo: "Rumours", artista: "Fleetwood Mac", genero: "Rock", anio: 1977, portada_url: "https://images.unsplash.com/photo-1511671782779-c97d3d27a1d4?w=300" },
+  { id: 13, titulo: "Confessions", artista: "Usher", genero: "R&B", anio: 2004, portada_url: "https://images.unsplash.com/photo-1465847899084-d164df4dedc6?w=300" },
+  { id: 14, titulo: "Blue Train", artista: "John Coltrane", genero: "Jazz", anio: 1958, portada_url: "https://images.unsplash.com/photo-1525417071004-5d41bc3ae290?w=300" },
+  { id: 15, titulo: "Live at the Regal", artista: "B.B. King", genero: "Blues", anio: 1965, portada_url: "https://images.unsplash.com/photo-1501386761578-eac5c94b800a?w=300" },
+  { id: 16, titulo: "Led Zeppelin IV", artista: "Led Zeppelin", genero: "Rock", anio: 1971, portada_url: "https://images.unsplash.com/photo-1510915228340-29c85a43dcfe?w=300" },
+  { id: 17, titulo: "Superfly", artista: "Curtis Mayfield", genero: "Soul", anio: 1972, portada_url: "https://images.unsplash.com/photo-1446057032654-9d8885b73941?w=300" },
+  { id: 18, titulo: "The Healer", artista: "John Lee Hooker", genero: "Blues", anio: 1989, portada_url: "https://images.unsplash.com/photo-1453090927415-5f45085b65c0?w=300" },
+  { id: 19, titulo: "Let's Get It On", artista: "Marvin Gaye", genero: "Soul", anio: 1973, portada_url: "https://images.unsplash.com/photo-1506157786151-b8491531f063?w=300" },
+  { id: 20, titulo: "Purple Rain", artista: "Prince", genero: "R&B", anio: 1984, portada_url: "https://images.unsplash.com/photo-1493225457124-a3eb161ffa5f?w=300" }
+];
+
+module.exports = vinilos;
